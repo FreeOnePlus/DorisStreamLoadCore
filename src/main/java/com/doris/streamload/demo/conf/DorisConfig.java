1a87a17bb1a210f857074f41649384b46e58a28d
Yijia Su <suyijia@selectdb.com>
2023-12-18 19:51:56 +0800

    streamload-master package

--- src/main/java/com/doris/streamload/demo/conf/DorisConfig.java
+++ src/main/java/com/doris/streamload/demo/conf/DorisConfig.java
@@ -20,7 +20,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Repository;
 
 /**
